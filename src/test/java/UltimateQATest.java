924eb9e364976f6c3b4aa6de5f3026d80d2898ac
Grzegorz Toruński <g.torunski@gmail.com>
2023-02-05 16:55:29 +0100

    Add TestThre and TestFour

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -2,11 +2,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class UltimateQATest extends PageSetup {
     @Test
-    public void testOne(){
+    public void testOne() {
         WebElement buttonUsingID = driver.findElement(By.id("idExample"));
         buttonUsingID.click();
 
@@ -33,8 +35,40 @@ public class UltimateQATest extends PageSetup {
         WebElement thanksForContactText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
         //      //*[] - postać xpath
         //      //div[@class='et-pb-contact-message']/p
+        Thread.sleep(1000);
         Assertions.assertEquals("Thanks for contacting us", thanksForContactText.getText());
 
+    }
+
+    @Test
+    public void testThree() {
+        List<WebElement> listOfQATitles = driver.findElements(By.xpath("//table[@id='htmlTableId']//tr"));
+        Assertions.assertEquals(4, listOfQATitles.size());
+
+        //      //table[@id='htmlTableId']//tr[4]/td[3]
+    }
+
+
+    @Test
+    public void testFour() {
+
+        List<String> listOfCars = new ArrayList<>();
+        listOfCars.add("volvo");
+        listOfCars.add("saab");
+        listOfCars.add("opel");
+        listOfCars.add("audi");
+        WebElement dropdownCars = driver.findElement(By.xpath("//div[text()='Select an option and validate that it is selected']/select"));
+
+        for(int i=0; i<listOfCars.size(); i++) {
+
+            dropdownCars.click();
+            WebElement carOption = driver.findElement(By.xpath("//option[@value='" + listOfCars.get(i) + "']"));
+            carOption.click();
+            Assertions.assertTrue(carOption.isSelected());
+            Assertions.assertEquals(listOfCars.get(i), carOption.getText().toLowerCase());
+        }
+    //      //input[@type='checkbox' and @value='Bike']
 
     }
+
 }
